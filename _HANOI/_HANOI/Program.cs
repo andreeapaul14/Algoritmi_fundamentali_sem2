a08d2683165066632d879857b2f59255cfeb02d3
Andreea <andreeapaul14@yahoo.ro>
2021-05-23 21:01:13 +0300

    HANOI

---
+++ _HANOI/_HANOI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _HANOI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi();
+        }
+        private static void Hanoi()
+        {
+            hanoi("source", "dest", "aux", 4);
+        }
+        static void hanoi(string source, string dest, string aux, int n)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(source + " -> " + dest);
+            }
+            else
+            {
+                hanoi(source, aux, dest, n - 1);
+                Console.WriteLine(source + " -> " + dest);
+                hanoi(aux, dest, source, n - 1);
+            }
+        }
+    }
+}
