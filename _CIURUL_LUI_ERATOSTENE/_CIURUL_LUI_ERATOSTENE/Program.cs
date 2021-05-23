e59de14fd7a71f993f7987983f436ab3d6406635
Andreea <andreeapaul14@yahoo.ro>
2021-05-23 21:12:55 +0300

    CIURUL_LUI_ERATOSTENE

---
+++ _CIURUL_LUI_ERATOSTENE/_CIURUL_LUI_ERATOSTENE/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _CIURUL_LUI_ERATOSTENE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CiurulLuiEratostene();
+        }
+        private static void CiurulLuiEratostene()
+        {
+            Console.WriteLine("**********Sieve of Eratosthenes********** ");
+            Console.WriteLine();
+            Console.Write("Enter a value for n: ");
+            n = Int32.Parse(Console.ReadLine());
+            array = new int[n + 1]; // In C# elementele unui array automat sunt initializate cu 0
+            ComputePrimeNumbers();
+            PrintPrimeNumbers();
+            Console.ReadKey();
+        }
+        public static int n { get; set; }
+        public static int[] array { get; set; }
+        static void PrintPrimeNumbers()
+        {
+            Console.Write($"The prime numbers up until {n} are: ");
+            for (int i = 2; i <= array.Length; i++)
+            {
+                if (array[i] == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+        static void ComputePrimeNumbers()
+        {
+            array[0] = 1;
+            array[1] = 1;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (array[i] == 0)
+                {
+                    for (int j = 2; j <= n / i; j++)
+                    {
+                        array[i * j] = 1;
+                    }
+                }
+            }
+        }
+    }
+}
